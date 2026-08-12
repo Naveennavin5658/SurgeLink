5b376f581254459b4378f3946ba1d533378b437e
Naveen <humblefool@Naveens-MacBook.local>
2026-08-12 21:29:39 +0530

    vercel url fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'surgelink-ui.onrender.com',
+    host: '0.0.0.0',
     port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
   },
   build: {
     outDir: 'dist',
